a6b9fd6eaf824483557ec1b3592bed5531128032
Álvaro Rodrigo <104114198+rodrigofr95@users.noreply.github.com>
2023-11-23 09:31:23 -0300

    Add files via upload

--- Site Interventoras/forms/inserir.php
+++ Site Interventoras/forms/inserir.php
@@ -3,15 +3,15 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Enviar</title>
+	<title>Cadastro</title>
 </head>
 <body>
 	<?php
 
 	$host 	= 'localhost';
 	$user 	= 'root';
-	$pass 	= '';
-	$db 	= 'si3dwa';
+	$pass 	= '1234';
+	$db 	= 'interventoras';
 
 	// Conecta ao banco de dados
 	$conn = mysqli_connect($host, $user, $pass, $db);
@@ -19,18 +19,17 @@
 	if (!$conn) {
 		
 		exit();
-		header("Location.form1.html");
+		header("Location.sign-up.html");
 
 	}
 
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		$nome = $_POST['nome'];
-		$telefone = $_POST['telefone'];
 		$email = $_POST['email'];
 		$senha = $_POST['senha'];
 
-		// Inserir os dados na tabelas 'usuarios'
-		$sql = "INSERT INTO usuarios (nome, telefone, email, senha) VALUES ('$nome', '$telefone', '$email', '$senha')";
+		// Inserir os dados na tabela 'cadastrodeusuarios'
+		$sql = "INSERT INTO cadastrodeusuarios (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
 
 		if(mysqli_query($conn, $sql)) {
 			echo "<h2>Registro inserido com sucesso!</h2>";
--- Site Interventoras/forms/login.php
+++ Site Interventoras/forms/login.php
@@ -1,6 +1,7 @@
+<?php
 // Verifique as credenciais (substitua isso com sua lógica de autenticação real)
-$valid_username = "seu_usuario";
-$valid_password = "sua_senha";
+$valid_username = "admin";
+$valid_password = "1234";
 
 if ($username === $valid_username && password_verify($password, $hashed_password)) {
     // Credenciais válidas, redirecione para a página principal
@@ -10,3 +11,4 @@ if ($username === $valid_username && password_verify($password, $hashed_password
     // Credenciais inválidas, exiba uma mensagem de erro
     $error_message = "Credenciais inválidas. Tente novamente.";
 }
+?>
